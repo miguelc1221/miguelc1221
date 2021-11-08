a092d030842ac75c67667aedda541d5329f35679
Miguel Correa <miguelc1221@gmail.com>
2021-11-07 17:18:47 -0700

    github actions

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ import { writeFileSync } from 'fs'
 import { readFile } from 'fs/promises'
 import { getAllComics } from './scrapeComics.js'
 
-const MUSTACHE_MAIN_DIR = './main.mustache'
+const MUSTACHE_DIR = './main.mustache'
 
 async function generateReadMe() {
   try {
-    const mustacheFile = await readFile(MUSTACHE_MAIN_DIR)
+    const mustacheFile = await readFile(MUSTACHE_DIR)
     const comics = await getAllComics()
 
-    const output = Mustache.render(data.toString(), { data: comics })
+    const output = Mustache.render(mustacheFile.toString(), {
+      data: comics,
+    })
     return writeFileSync('README.md', output)
   } catch (error) {
     console.log(error)
